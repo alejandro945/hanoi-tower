b093b2dc96ac574f90c5a7a8faebadce4e01232d
Alejandro Varela <alejo8677@gmail.com>
2021-04-16 17:08:44 -0500

    We have fixed some bugs

--- README.md
+++ README.md
@@ -1,14 +1,11 @@
-## Getting Started
+## Hanoi Tower Proyect
 
-Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
+Project with emphasis on the use of recursion, that solves showing the amount of disks at the moment by CLI.
 
 ## Folder Structure
 
 The workspace contains two folders by default, where:
 
 - `src`: the folder to maintain sources
-- `lib`: the folder to maintain dependencies
+- `data`: the folder to maintain data
 
-## Dependency Management
-
-The `JAVA DEPENDENCIES` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-pack/blob/master/release-notes/v0.9.0.md#work-with-jar-files-directly).
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -10,28 +10,99 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         cases = new ArrayList<>();
-        readProblem(br);
-        exportSolution();
+        int option;
+        bw.write("1. CLI Console" + "\n");
+        bw.write("2. Import and Export" + "\n");
+        bw.write("Enter the option: ");
+        bw.flush();
+        option = Integer.parseInt(br.readLine());
+        menu(option);
         br.close();
         bw.close();
     }
 
-    public static void readProblem(BufferedReader br) throws IOException {
+    public static void readProblem() throws IOException {
         String casesNumber = br.readLine();
-        readCases(br, Integer.parseInt(casesNumber));
+        readCases(Integer.parseInt(casesNumber));
     }
 
-    public static void readCases(BufferedReader br, int render) throws IOException {
+    public static void readCases(int render) throws IOException {
         if (render > 0) {
             String occasion = br.readLine();
             cases.add(Integer.parseInt(occasion));
-            readCases(br, render - 1);
+            readCases(render - 1);
         }
+    }
+
+    public static ArrayList<String> WriteSolution(int redux, ArrayList<String> data, int option) throws IOException {
+        if (redux < cases.size()) {
+            int[] render = new int[3];
+            render[0] = cases.get(redux);
+            if (option == 1) {
+                // --------------------------------CONSOLE-------------------------------
+                bw.write("\n" + render[0] + " " + render[1] + " " + render[2] + "\n");
+                bw.flush();
+                hanoi(cases.get(redux), 0, 1, 2, render, null, option);
+            }
+            if (option == 2) {
+                // --------------------------------EXPORT--------------------------------
+                data.add((redux == 0) ? render[0] + " " + render[1] + " " + render[2] + "\n"
+                        : "\n" + render[0] + " " + render[1] + " " + render[2] + "\n");
+                ArrayList<String> concat = new ArrayList<>();
+                concat = hanoi(cases.get(redux), 0, 1, 2, render, concat, option);
+                for (String string : concat) {
+                    data.add(string);
+                }
+            }
+            WriteSolution(redux + 1, data, option);
+        }
+        return data;
+    }
+
+    public static ArrayList<String> hanoi(int cases, int init, int medium, int end, int[] render,
+            ArrayList<String> concat, int option) throws IOException {
+        if (cases > 0) {
+            hanoi(cases - 1, init, end, medium, render, concat, option);
+            render[init]--;
+            render[end]++;
+            if (option == 1) {
+                // --------------------------------CONSOLE-------------------------------
+                bw.write(render[0] + " " + render[1] + " " + render[2] + "\n");
+                bw.flush();
+                hanoi(cases - 1, medium, init, end, render, concat, option);
+            }
+            if (option == 2) {
+                // --------------------------------EXPORT--------------------------------
+                concat.add((render[0] + " " + render[1] + " " + render[2] + "\n"));
+                hanoi(cases - 1, medium, init, end, render, concat, option);
+            }
+        }
+        return concat;
+    }
+
+    public static void importData() throws NumberFormatException, IOException {
+        // IMPORT
+        BufferedReader brf = new BufferedReader(new FileReader("data/Hanoi_input.txt"));
+        int numCases = Integer.parseInt(brf.readLine());
+        while (numCases > 0) {
+            cases.add(Integer.parseInt(brf.readLine()));
+            numCases--;
+        }
+        brf.close();
+    }
+
+    public static void exportSolution() throws IOException {
+        // EXPORT
+        BufferedWriter bwf = new BufferedWriter(new FileWriter("data/Output.txt"));
+        bwf.write(getCases());
+        bwf.close();
+        bw.write("Data have been imported and exported succesfully, CHECK OUTPUT.TXT IN DATA FOLDER");
+        bw.flush();
     }
 
     public static String getCases() throws IOException {
         ArrayList<String> c = new ArrayList<>();
-        WriteSolution(bw, 0, c);
+        WriteSolution(0, c, 2);
         String data = "";
         for (String string : c) {
             data += string;
@@ -39,48 +110,16 @@ public class Main {
         return data;
     }
 
-    public static ArrayList<String> WriteSolution(BufferedWriter bw, int redux, ArrayList<String> data)
-            throws IOException {
-        if (redux < cases.size()) {
-            int[] render = new int[3];
-            render[0] = cases.get(redux);
-            // --------------------------------CONSOLE-------------------------------
-            bw.write("\n" + render[0] + " " + render[1] + " " + render[2] + "\n");
-            bw.flush();
-            // --------------------------------EXPORT--------------------------------
-            data.add((redux == 0) ? render[0] + " " + render[1] + " " + render[2] + "\n"
-                    : "\n" + render[0] + " " + render[1] + " " + render[2] + "\n");
-            ArrayList<String> concat = new ArrayList<>();
-            concat = hanoi(cases.get(redux), 0, 1, 2, render, concat);
-            for (String string : concat) {
-                data.add(string);
-            }
-            WriteSolution(bw, redux + 1, data);
+    public static void menu(int option) throws IOException {
+        switch (option) {
+        case 1:
+            readProblem();
+            WriteSolution(0, null, 1);
+            break;
+        case 2:
+            importData();
+            exportSolution();
+            break;
         }
-        return data;
-    }
-
-    public static ArrayList<String> hanoi(int cases, int init, int medium, int end, int[] render,
-            ArrayList<String> concat) throws IOException {
-        if (cases > 0) {
-            hanoi(cases - 1, init, end, medium, render, concat);
-            render[init]--;
-            render[end]++;
-            // --------------------------------CONSOLE-------------------------------
-            bw.write(render[0] + " " + render[1] + " " + render[2] + "\n");
-            bw.flush();
-            // --------------------------------EXPORT--------------------------------
-            concat.add((render[0] + " " + render[1] + " " + render[2] + "\n"));
-            hanoi(cases - 1, medium, init, end, render, concat);
-        }
-        return concat;
-    }
-
-    public static void exportSolution() throws IOException {
-        BufferedWriter bwf = new BufferedWriter(new FileWriter("data/Output.txt"));
-        bwf.write(getCases());
-        bwf.close();
-        bw.write("Data have been imported and exported succesfully, CHECK OUTPUT.TXT IN DATA FOLDER");
-        bw.flush();
     }
 }
